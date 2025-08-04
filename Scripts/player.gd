25ed98ec98346ef1e978772fd27074f14ea875f2
Natelytle <nathenbr@gmail.com>
2025-08-04 02:17:12 -0400

    clean up

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -3,7 +3,7 @@ extends CharacterBody3D
 @onready var hitbox: CollisionShape3D = $Hitbox
 
 # movement
-const MAX_SPEED = 16.0
+const MAX_SPEED = 125.0
 const GROUND_ACCELERATION = 50
 const AIR_ACCELERATION = 15
 const JUMP_VELOCITY = 50
@@ -35,16 +35,14 @@ func _input(event: InputEvent) -> void:
 		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 
 
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	if mouse_locked:
 		hitbox.rotation.y = camera.rotation.y
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
-	# Handle jump.
 	if Input.is_action_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
@@ -54,7 +52,6 @@ func _physics_process(delta: float) -> void:
 	
 	var direction = (transform.basis.rotated(Vector3.UP, camera.rotation.y) * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 
-	# basis rotated by the camera
 	if is_on_floor():
 		var blend = 1 - pow(0.5, GROUND_ACCELERATION * delta)
 		currentMovementVector = lerp(currentMovementVector, direction, blend)
@@ -62,11 +59,7 @@ func _physics_process(delta: float) -> void:
 		var blend = 1 - pow(0.5, AIR_ACCELERATION * delta)
 		currentMovementVector = lerp(currentMovementVector, direction, blend)
 
-	# if direction:
 	velocity.x = currentMovementVector.x * MAX_SPEED
 	velocity.z = currentMovementVector.z * MAX_SPEED
-	#else:
-		#velocity.x = move_toward(velocity.x, 0, SPEED)
-		#velocity.z = move_toward(velocity.z, 0, SPEED)
 
 	move_and_slide()
