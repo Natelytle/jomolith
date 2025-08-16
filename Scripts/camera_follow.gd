9f782206d74a014d1c1ecdbbaf04f38f634fc42d
Natelytle <nathenbr@gmail.com>
2025-08-15 21:02:44 -0400

    Rewrite it in C#!!!!!!

--- Scripts/camera_follow.gd
+++
@@ -1,8 +0,0 @@
-extends Camera3D
-
-@onready
-var spring_arm := $"../PlayerCamera/SpringPosition"
-var lerp_power: float = 20.0
-
-func _process(delta: float) -> void:
-	position = lerp(position, spring_arm.position, delta*lerp_power)
