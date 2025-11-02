7997ee23b9d727c95fb5af28146df585986e83a4
Nathen <nathenbr@gmail.com>
2025-11-01 22:15:46 -0400

    Start editor UI

---
+++ Scripts/Editor/EditorUI.cs
@@ -0,0 +1,26 @@
+using System;
+using Godot;
+using static Jomolith.Scripts.Editor.EditorController;
+
+namespace Jomolith.Scripts.Editor;
+
+public partial class EditorUI : Control
+{
+	private EditorController? _editorController;
+
+	public override void _Ready()
+	{
+		_editorController = (EditorController)GetParent();
+
+		((Button)GetNode("Panel/VBoxContainer/SelectButton")).Pressed += () => OnModeButtonPressed(EditorMode.Select);
+		((Button)GetNode("Panel/VBoxContainer/MoveButton")).Pressed += () => OnModeButtonPressed(EditorMode.Translate);
+		((Button)GetNode("Panel/VBoxContainer/RotateButton")).Pressed += () => OnModeButtonPressed(EditorMode.Rotate);
+		((Button)GetNode("Panel/VBoxContainer/ScaleButton")).Pressed += () => OnModeButtonPressed(EditorMode.Scale);
+	}
+	
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	private void OnModeButtonPressed(EditorMode mode)
+	{
+		_editorController?.ChangeEditorMode(mode);
+	}
+}
