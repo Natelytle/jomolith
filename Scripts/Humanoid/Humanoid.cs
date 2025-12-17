aef628aaec5abeee1f07f9f6576bc5920259c68e
Natelytle <nathenbr@gmail.com>
2025-12-16 22:34:02 -0500

    Adjust animation blend time

--- Scripts/Humanoid/Humanoid.cs
+++ Scripts/Humanoid/Humanoid.cs
@@ -66,6 +66,8 @@ public partial class Humanoid : RigidBody3D
 		AnimationPlayer = (AnimationPlayer)GetNode("Avatar/AnimationPlayer");
 		
 		CurrentState = GetState(InitialState);
+		
+		AnimationPlayer.SetDefaultBlendTime(0.1);
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
